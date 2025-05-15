ae6b9fe687fa9312b7eb458083f74bb436e3692c
ZiggyAdriatico <za23003@mbc.school.nz>
2025-05-16 09:23:02 +1200

    Lesson 1 complete

--- starter_activity_sheet_1.py
+++ starter_activity_sheet_1.py
@@ -6,10 +6,10 @@ print("We want to know if you like progamming!")
 print()
 # combine the next 2 lines into one commeand
 
-answer = input(f"Do you like programming " + name + "? ")
+answer = input(f"Do you like programming {name}? ")
 
 # change to an f string
-print(f"Great! You said " + answer + "!")
+print(f"Great! You said {answer}!")
 
 print(f"Let's learn some Python today.")
 
